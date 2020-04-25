77467669ecc07facfd2924b6b302cc644a05d408
Patrick <64305846+LasVexe@users.noreply.github.com>
2020-04-25 15:05:00 +0200

    Smooth Scrolling

--- js/main.js
+++ js/main.js
@@ -118,6 +118,26 @@
       }
   	}
 	});
+    
+    // smooth scrolling
+    $('body').scrollspy({target: ".navbar", offset: 50});
+
+    $("#myNavbar a").on('click', function(event) {
+
+        if (this.hash !== "") {
+            event.preventDefault();
+        
+            var hash = this.hash;
+        
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+                }, 800, function(){
+                    window.location.hash = hash;
+            });
+
+        }
+
+    }); 
 
 
 	var contentWayPoint = function() {
